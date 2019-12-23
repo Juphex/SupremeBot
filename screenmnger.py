29dfc078d273b5ca296cd6b20c4c98ba862142e3
Juphex <juphex@aol.de>
2019-12-23 21:23:09 +0100

    test things Please enter the commit message for your changes. Lines starting

--- screenmnger.py
+++ screenmnger.py
@@ -2,6 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+from kivy.uix.actionbar import ActionPrevious, ActionButton, ActionBar, ActionGroup, ActionView, ActionItem
+from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
+
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
@@ -13,6 +17,24 @@ class MenuScreen(Screen):
         self.btn = Button(text="Menu Screen")
         self.btn.bind(on_press = self.switch_screen)
         self.add_widget(self.btn)
+
+        #action bar
+        self.actnbr = ActionBar(pos_hint = {"top" : 1})
+        #self.drpdwn = DropDown()
+        #self.drpdwn.add_widget(Label(text=u'Hello world ' + chr(2764)))
+        self.actnvw = ActionView(use_separator=True)
+        #self.actitm = ActionItem()
+        #self.actitm.add_widget(self.drpdwn)
+        self.group = ActionGroup(text = "Supreme App")
+        self.group.add_widget(ActionButton(text="hallo"))
+        self.actnvw.add_widget(self.group)
+
+        #app_icon in ActionPrevious
+        #on_press: print(self)
+        self.actnprv = ActionPrevious(inside_group=True,title="ActPrevious", with_previous=False)
+        self.actnvw.add_widget(self.actnprv)
+        self.actnbr.add_widget(self.actnvw)
+        self.add_widget(self.actnbr)
 
     def switch_screen(self, instance):
        print("")
