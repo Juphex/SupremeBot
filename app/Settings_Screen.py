0de92ccc8b0455623892d6d87e13fe5a0908ba18
Chris <>
2019-12-26 18:43:51 +0100

    app structure

---
+++ app/Settings_Screen.py
@@ -0,0 +1,11 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+
+class SettingsScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.btn = Button(text="Menu Screen")
+        self.btn.bind(on_press = self.switch_screen)
+        self.add_widget(self.btn)
+    def switch_screen(self, instance):
+        self.manager.current = "menu"
