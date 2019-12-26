2da82f22ae53ad871966d618c31442fe9a0675d2
Chris <>
2019-12-27 00:25:29 +0100

    basic gui

--- app/App.py
+++ app/App.py
@@ -1,9 +1,9 @@
 import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from login import Login
-from Menu_Screen import MenuScreen
+from kivy.uix.screenmanager import ScreenManager
+from View_Screen import ViewScreen
 from Settings_Screen import SettingsScreen
+from Login_screen import LoginScreen
 
 class SupremeBot(App):
     def build(self):
@@ -11,7 +11,9 @@ class SupremeBot(App):
         
 if __name__ == "__main__":
     sm = ScreenManager()
-    sm.add_widget(MenuScreen(name='menu'))
+
+    sm.add_widget(LoginScreen(name="login"))
+    sm.add_widget(ViewScreen(name='view'))
     sm.add_widget(SettingsScreen(name='settings'))
 
     SupremeBot().run()
