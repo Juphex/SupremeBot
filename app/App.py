139cdbec120d53b2cc8846caaf0e57073ebf48fb
Chris <>
2019-12-26 18:43:51 +0100

    app structure

--- app/App.py
+++ app/App.py
@@ -2,30 +2,16 @@ import kivy
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from login import Login
-from dashboard import Dashboard
-
-class LoginScreen(Screen):
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        login_page = Login()
-        self.add_widget(login_page)
-
-class DashboardScreen(Screen):
-    def __init__(self, **kwargs):
-        super(DashboardScreen, self).__init__(**kwargs)
-        dashboard = Dashboard("sd")
-        #self.add_widget(dashboard)
-
-
+from Menu_Screen import MenuScreen
+from Settings_Screen import SettingsScreen
 
 class SupremeBot(App):
     def build(self):
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(LoginScreen(name="Login"))
-        screen_manager.add_widget(DashboardScreen(name="Dashboard"))
-        screen_manager.current = "Login"
-
-        return screen_manager
+        return sm
         
 if __name__ == "__main__":
+    sm = ScreenManager()
+    sm.add_widget(MenuScreen(name='menu'))
+    sm.add_widget(SettingsScreen(name='settings'))
+
     SupremeBot().run()
