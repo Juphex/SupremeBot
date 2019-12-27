5c1ee35c746fbb8afc04a0317e25f3d7b63a449e
Chris <>
2019-12-27 15:40:33 +0100

    added info to items

--- crawler/get_info.py
+++ crawler/get_info.py
@@ -3,11 +3,39 @@ from crawler import Crawler
 #adds further infos to given items
 #status=None, sizes=None, price=None
 class Get_Info(Crawler):
+    def __init__(self):
+        super(Get_Info, self).__init__()
+
+    #Items can be one item or list
     def get(self, items):
-        for item in items:
-            if item[0] == None:
-                request = self.rq.get(item[1])
-                response = self.TextResponse(item[1], body=request.content)
-                item[0] = response.css("p[itemprop=description]::text").get()
+        #determine wether items is list
+        if len(items) > 1:
+            for item in items:
+                #check of sold out
+                url = item.link
+                request = self.rq.get(url)
+                response = self.TextResponse(url, body=request.content)
+                item.name = response.css("div > h1[itemprop='name']::text").get()
+                item.model = response.css("div > p[itemprop='model']::text").get()
+                item.price = response.css("div > p[itemprop='model']::text").get()
+                item.sizes = response.css("select[id='size'] > option").get()
+        else:
+                url = items.link
+                request = self.rq.get(url)
+                response = self.TextResponse(url, body=request.content)
+                items.name = response.css("div > h1[itemprop='name']::text").get()
+                items.model = response.css("div > p[itemprop='model']::text").get()
+                items.price = response.css("div > p[itemprop='model']::text").get()
+                items.sizes = response.css("select[id='size'] > option").get()
+
         return items
 
+    #TESTMETHOD
+    def get_url(self,url):
+        request = self.rq.get(url)
+        response = self.TextResponse(url, body=request.content)
+        print(response.css("div > h1[itemprop='name']::text").get())
+        print(response.css("div > p[itemprop='model']::text").get())
+        print(response.css("p > span[itemprop='price']::text").get())
+        print(response.css("select[id='size'] > option").get())
+        
--- crawler/item.py
+++ crawler/item.py
@@ -1,5 +1,6 @@
 class Item():
-    def __init__(self, name, link, img_src, status=None, sizes=None, price=None, category=None):
+    def __init__(self, name, link, img_src, status=None, sizes=None, price=None, category=None,\
+        model=None):
         self.name = name
         self.link = link
         self.img_src = img_src
@@ -7,3 +8,15 @@ class Item():
         self.sizes = sizes
         self.price = price
         self.category = category
+        self.model = model
+    
+    def sold_out(self):
+        #check if working
+        if sold_out == "sold out":
+            return True
+
+        return False
+    
+    #Override
+    def __len__(self):
+        return 1
