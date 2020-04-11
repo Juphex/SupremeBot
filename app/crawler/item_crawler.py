88fcaec74a6cfa7729cd3fae909c83d1ecb7e043
Juphex <20795522+Juphex@users.noreply.github.com>
2020-04-11 20:55:56 +0200

    changed crawler link

--- app/crawler/item_crawler.py
+++ app/crawler/item_crawler.py
@@ -5,7 +5,7 @@ from .categories import Categories
 class Item_Crawler():
     def __init__(self):
         self.categories = Categories
-        self.get_items = Get_Items("https://www.supremenewyork.com/")
+        self.get_items = Get_Items("https://www.supremenewyork.com/shop/shirts")
         self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/shirts")
         #ADD CATEGORIES
         self.get_info = Get_Info()
