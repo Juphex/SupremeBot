bfa5de2ea12f11dca9e3d57f5c1b5b0e4030c029
Juphex <20795522+Juphex@users.noreply.github.com>
2020-04-11 22:59:31 +0200

    enabled ssl, i.e. https

--- app/crawler/crawler.py
+++ app/crawler/crawler.py
@@ -1,6 +1,10 @@
 import requests as rq
 from scrapy.http import TextResponse
 
+#enable to load https for e.g. AsyncImage
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 class Crawler:
     def __init__(self):
         self.rq = rq
