2c9a96d9d4f55c7302f21f72e1f765a25a61a812
Juphex <20795522+Juphex@users.noreply.github.com>
2020-04-11 21:02:44 +0200

    fixed url

--- app/crawler/item_crawler.py
+++ app/crawler/item_crawler.py
@@ -5,12 +5,12 @@ from .categories import Categories
 class Item_Crawler():
     def __init__(self):
         self.categories = Categories
-        self.get_items = Get_Items("https://www.supremenewyork.com/shop/shirts")
-        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/shirts")
+        self.get_items = Get_Items("https://www.supremenewyork.com/shop/all/shirts")
+        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all/shirts")
         #ADD CATEGORIES
         self.get_info = Get_Info()
         self.items_all = self.get_info.get(self.items_all)
 
     def refresh(self, category):
-        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/shirts")
+        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all/shirts")
         self.items_all = self.get_info.get(self.items_all)
