4d4e7467cdc76cc2dd40d46d8f01ce182e05c000
Juphex <20795522+Juphex@users.noreply.github.com>
2020-04-11 19:24:35 +0200

    restored crawler

--- app/crawler/item_crawler.py
+++ app/crawler/item_crawler.py
@@ -5,12 +5,12 @@ from .categories import Categories
 class Item_Crawler():
     def __init__(self, url):
         self.categories = Categories
-     #   self.get_items = Get_Items("https://www.supremenewyork.com/")
-    #    self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all")
+        self.get_items = Get_Items("https://www.supremenewyork.com/")
+        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all")
         #ADD CATEGORIES
-     #   self.get_info = Get_Info()
-    #    self.items_all = self.get_info.get(self.items_all)
+        self.get_info = Get_Info()
+        self.items_all = self.get_info.get(self.items_all)
 
-    #def refresh(self, category):
-     #   self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all")
-     #   self.items_all = self.get_info.get(self.items_all)
+    def refresh(self, category):
+        self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all")
+        self.items_all = self.get_info.get(self.items_all)
