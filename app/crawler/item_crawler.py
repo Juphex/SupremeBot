3ef86d01e21129b5015e82caab14c1a36dc31aab
Juphex <20795522+Juphex@users.noreply.github.com>
2020-04-11 19:22:44 +0200

    hide refresh method

--- app/crawler/item_crawler.py
+++ app/crawler/item_crawler.py
@@ -11,6 +11,6 @@ class Item_Crawler():
      #   self.get_info = Get_Info()
     #    self.items_all = self.get_info.get(self.items_all)
 
-    def refresh(self, category):
+    #def refresh(self, category):
      #   self.items_all = self.get_items.get_items("https://www.supremenewyork.com/shop/all")
      #   self.items_all = self.get_info.get(self.items_all)
